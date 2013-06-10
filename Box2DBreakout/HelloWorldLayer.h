9b5a6fea39986d44da7f32c2d97a5d50d96ec175
Jean Sung <jsung@g.hmc.edu>
2013-06-10 14:57:26 -0700

    finished game

--- Box2DBreakout/HelloWorldLayer.h
+++ Box2DBreakout/HelloWorldLayer.h
@@ -8,6 +8,7 @@
 
 #import "cocos2d.h"
 #import "Box2D.h"
+#import "MyContactListener.h"
 
 #define PTM_RATIO 32.0
 
@@ -16,6 +17,11 @@
     b2Body *_groundBody;
     b2Fixture *_bottomFixture;
     b2Fixture *_ballFixture;
+    b2Body *_paddleBody;
+    b2Fixture *_paddleFixture;
+    b2MouseJoint *_mouseJoint;
+    MyContactListener *_contactListener;
+
 
 }
 
--- Box2DBreakout/HelloWorldLayer.mm
+++ Box2DBreakout/HelloWorldLayer.mm
@@ -7,7 +7,8 @@
 //
 
 #import "HelloWorldLayer.h"
-
+#import "GameOverLayer.h"
+#import "SimpleAudioEngine.h"
 
 @implementation HelloWorldLayer
 
@@ -20,18 +21,18 @@
     
 }
 
--(id) init
-{
-	if( (self=[super init])) {
+- (id)init {
+    
+    if ((self=[super init])) {
         CGSize winSize = [CCDirector sharedDirector].winSize;
         
-        //create a world
+        // Create a world
         b2Vec2 gravity = b2Vec2(0.0f, 0.0f);
         _world = new b2World(gravity);
         
-        //create edges around the entire screen
+        // Create edges around the entire screen
         b2BodyDef groundBodyDef;
-        groundBodyDef.position.Set(0, 0);
+        groundBodyDef.position.Set(0,0);
         _groundBody = _world->CreateBody(&groundBodyDef);
         
         b2EdgeShape groundBox;
@@ -39,8 +40,6 @@
         groundBoxDef.shape = &groundBox;
         
         groundBox.Set(b2Vec2(0,0), b2Vec2(winSize.width/PTM_RATIO, 0));
-        
-        //reference to the bottom, need for gameplay layer
         _bottomFixture = _groundBody->CreateFixture(&groundBoxDef);
         
         groundBox.Set(b2Vec2(0,0), b2Vec2(0, winSize.height/PTM_RATIO));
@@ -53,7 +52,6 @@
         groundBox.Set(b2Vec2(winSize.width/PTM_RATIO, winSize.height/PTM_RATIO),
                       b2Vec2(winSize.width/PTM_RATIO, 0));
         _groundBody->CreateFixture(&groundBoxDef);
-        
         
         // Create sprite and add it to the layer
         CCSprite *ball = [CCSprite spriteWithFile:@"ball.png"];
@@ -80,9 +78,241 @@
         ballShapeDef.restitution = 1.0f;
         _ballFixture = ballBody->CreateFixture(&ballShapeDef);
         
+        b2Vec2 force = b2Vec2(10, 10);
+        ballBody->ApplyLinearImpulse(force, ballBodyDef.position);
         
-	}
-	return self;
+        [self schedule:@selector(tick:)];
+        
+        // Create paddle and add it to the layer
+        CCSprite *paddle = [CCSprite spriteWithFile:@"paddle.png"];
+        paddle.position = ccp(winSize.width/2, 50);
+        [self addChild:paddle];
+        
+        // Create paddle body
+        b2BodyDef paddleBodyDef;
+        paddleBodyDef.type = b2_dynamicBody;
+        paddleBodyDef.position.Set(winSize.width/2/PTM_RATIO, 50/PTM_RATIO);
+        paddleBodyDef.userData = paddle;
+        _paddleBody = _world->CreateBody(&paddleBodyDef);
+        
+        // Create paddle shape
+        b2PolygonShape paddleShape;
+        paddleShape.SetAsBox(paddle.contentSize.width/PTM_RATIO/2,
+                             paddle.contentSize.height/PTM_RATIO/2);
+        
+        // Create shape definition and add to body
+        b2FixtureDef paddleShapeDef;
+        paddleShapeDef.shape = &paddleShape;
+        paddleShapeDef.density = 10.0f;
+        paddleShapeDef.friction = 0.4f;
+        paddleShapeDef.restitution = 0.1f;
+        _paddleFixture = _paddleBody->CreateFixture(&paddleShapeDef);
+        
+        self.isTouchEnabled = YES;
+        
+        // Restrict paddle along the x axis
+        b2PrismaticJointDef jointDef;
+        b2Vec2 worldAxis(1.0f, 0.0f);
+        jointDef.collideConnected = true;
+        jointDef.Initialize(_paddleBody, _groundBody,
+                            _paddleBody->GetWorldCenter(), worldAxis);
+        _world->CreateJoint(&jointDef);
+        
+        //create contact listener
+        _contactListener = new MyContactListener();
+        _world->SetContactListener(_contactListener);
+        
+        //create row of blocks
+        for(int i = 0; i < 4; i++) {
+            
+            static int padding=20;
+            
+            // Create block and add it to the layer
+            CCSprite *block = [CCSprite spriteWithFile:@"block.png"];
+            int xOffset = padding+block.contentSize.width/2+
+            ((block.contentSize.width+padding)*i);
+            block.position = ccp(xOffset, 250);
+            block.tag = 2;
+            [self addChild:block];
+            
+            // Create block body
+            b2BodyDef blockBodyDef;
+            blockBodyDef.type = b2_dynamicBody;
+            blockBodyDef.position.Set(xOffset/PTM_RATIO, 250/PTM_RATIO);
+            blockBodyDef.userData = block;
+            b2Body *blockBody = _world->CreateBody(&blockBodyDef);
+            
+            // Create block shape
+            b2PolygonShape blockShape;
+            blockShape.SetAsBox(block.contentSize.width/PTM_RATIO/2,
+                                block.contentSize.height/PTM_RATIO/2);
+            
+            // Create shape definition and add to body
+            b2FixtureDef blockShapeDef;
+            blockShapeDef.shape = &blockShape;
+            blockShapeDef.density = 10.0;
+            blockShapeDef.friction = 0.0;
+            blockShapeDef.restitution = 0.1f;
+            blockBody->CreateFixture(&blockShapeDef);
+            
+            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"background-music-aac.caf"];
+
+            
+        }
+        
+    }
+    return self;
+}
+
+- (void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    if (_mouseJoint != NULL) return;
+    
+    UITouch *myTouch = [touches anyObject];
+    CGPoint location = [myTouch locationInView:[myTouch view]];
+    location = [[CCDirector sharedDirector] convertToGL:location];
+    b2Vec2 locationWorld = b2Vec2(location.x/PTM_RATIO, location.y/PTM_RATIO);
+    
+    if (_paddleFixture->TestPoint(locationWorld)) {
+        b2MouseJointDef md;
+        md.bodyA = _groundBody;
+        md.bodyB = _paddleBody;
+        md.target = locationWorld;
+        md.collideConnected = true;
+        md.maxForce = 1000.0f * _paddleBody->GetMass();
+        
+        _mouseJoint = (b2MouseJoint *)_world->CreateJoint(&md);
+        _paddleBody->SetAwake(true);
+    }
+    
+}
+
+-(void)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    if (_mouseJoint == NULL) return;
+    
+    UITouch *myTouch = [touches anyObject];
+    CGPoint location = [myTouch locationInView:[myTouch view]];
+    location = [[CCDirector sharedDirector] convertToGL:location];
+    b2Vec2 locationWorld = b2Vec2(location.x/PTM_RATIO, location.y/PTM_RATIO);
+    
+    _mouseJoint->SetTarget(locationWorld);
+    
+}
+
+-(void)ccTouchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    if (_mouseJoint) {
+        _world->DestroyJoint(_mouseJoint);
+        _mouseJoint = NULL;
+    }
+    
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (_mouseJoint) {
+        _world->DestroyJoint(_mouseJoint);
+        _mouseJoint = NULL;
+    }
+}
+
+- (void)tick:(ccTime) dt {
+    bool blockFound = false;
+    _world->Step(dt, 10, 10);
+    for(b2Body *b = _world->GetBodyList(); b; b=b->GetNext()) {
+        if (b->GetUserData() != NULL) {
+            CCSprite *sprite = (CCSprite *)b->GetUserData();
+            if (sprite.tag == 2) {
+                blockFound = true;
+            }
+            
+            // if ball is going too fast, turn on damping
+            if (sprite.tag == 1) {
+                static int maxSpeed = 10;
+                
+                b2Vec2 velocity = b->GetLinearVelocity();
+                float32 speed = velocity.Length();
+                
+                if (speed > maxSpeed) {
+                    b->SetLinearDamping(0.5);
+                } else if (speed < maxSpeed) {
+                    b->SetLinearDamping(0.0);
+                }
+                
+            }
+            
+            sprite.position = ccp(b->GetPosition().x * PTM_RATIO,
+                                  b->GetPosition().y * PTM_RATIO);
+            sprite.rotation = -1 * CC_RADIANS_TO_DEGREES(b->GetAngle());
+        }
+    }
+    
+    
+    //check if ball hit bottom, lose screen if so
+    std::vector<b2Body *>toDestroy;
+    std::vector<MyContact>::iterator pos;
+    for (pos=_contactListener->_contacts.begin();
+         pos != _contactListener->_contacts.end(); ++pos) {
+        MyContact contact = *pos;
+        
+        if ((contact.fixtureA == _bottomFixture && contact.fixtureB == _ballFixture) ||
+            (contact.fixtureA == _ballFixture && contact.fixtureB == _bottomFixture)) {
+            //NSLog(@"Ball hit bottom!");
+            CCScene *gameOverScene = [GameOverLayer sceneWithWon:NO];
+            [[CCDirector sharedDirector] replaceScene:gameOverScene];
+        }
+        
+        b2Body *bodyA = contact.fixtureA->GetBody();
+        b2Body *bodyB = contact.fixtureB->GetBody();
+        if (bodyA->GetUserData() != NULL && bodyB->GetUserData() != NULL) {
+            CCSprite *spriteA = (CCSprite *) bodyA->GetUserData();
+            CCSprite *spriteB = (CCSprite *) bodyB->GetUserData();
+            
+            //Sprite A = ball, Sprite B = Block
+            if (spriteA.tag == 1 && spriteB.tag == 2) {
+                if (std::find(toDestroy.begin(), toDestroy.end(), bodyB) == toDestroy.end()) {
+                    toDestroy.push_back(bodyB);
+                }
+            }
+            
+            //Sprite A = block, Sprite B = ball
+            else if (spriteA.tag == 2 && spriteB.tag == 1) {
+                if (std::find(toDestroy.begin(), toDestroy.end(), bodyA) == toDestroy.end()) {
+                    toDestroy.push_back(bodyA);
+                }
+            }
+        }
+    }
+    
+    std::vector<b2Body *>::iterator pos2;
+    for (pos2 = toDestroy.begin(); pos2 != toDestroy.end(); ++pos2) {
+        b2Body *body = *pos2;
+        if (body->GetUserData() != NULL) {
+            CCSprite *sprite = (CCSprite *) body->GetUserData();
+            [self removeChild:sprite cleanup:YES];
+        }
+        _world->DestroyBody(body);
+    }
+    
+    if (!blockFound) {
+        CCScene *gameOverScene = [GameOverLayer sceneWithWon:YES];
+        [[CCDirector sharedDirector] replaceScene:gameOverScene];
+    }
+    
+    if (toDestroy.size() > 0) {
+        [[SimpleAudioEngine sharedEngine] playEffect:@"blip.caf"];
+    }
+
+
+}
+
+- (void)dealloc {
+    
+    delete _world;
+    _groundBody = NULL;
+    delete _contactListener;
+    [super dealloc];
+    
 }
 
 @end
